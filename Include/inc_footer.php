f750f6806514f49c7f7674d991510d331ebe2311
ClaeymanDamien <damien.claeyman.20160288@efrei.net>
2018-10-06 04:22:55 +0200

    Copyright

--- Include/inc_footer.php
+++ Include/inc_footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <span class="copyright">&copy; Created by Martin DE COMBARIEU and Louis-Victor LAMY</span>
+                <span class="copyright">&copy; Created by Martin DE COMBARIEU, Louis-Victor LAMY & Damien CLAEYMAN</span>
             </div>
         </div>
     </div>
